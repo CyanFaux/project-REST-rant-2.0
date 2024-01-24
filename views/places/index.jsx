734a48c34350dc3ab82011246f55a8604aca93ef
CyanFaux <santafox26@gmail.com>
2024-01-23 21:23:30 -0500

    following activity 3 & css styles not working

---
+++ views/places/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+          </main>
+      </Def>
+    )
+  }
+
+module.exports = index
