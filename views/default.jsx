3766ffac1aa7a8df58bddd82ebe062d71b8b82c0
CyanFaux <santafox26@gmail.com>
2024-01-25 18:34:28 -0500

    fixed some big stuff

--- views/default.jsx
+++ views/default.jsx
@@ -7,9 +7,6 @@ function Def (html) {
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
                 <link rel="stylesheet" href="/css/style.css"/>
-                <a href="/places">
-                <button className="btn-primary">Places Page</button>
-                </a>
             </head>
             <body>
                 {html.children}
