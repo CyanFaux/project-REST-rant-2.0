f81d2fdf3fecd07ada910c0a7d71789dfc221ef5
CyanFaux <santafox26@gmail.com>
2024-02-01 20:10:07 -0500

    Show route connected

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,11 @@ const React = require('react')
 const Def = require('../default')
 
 function show () {
-    <Def></Def>
+    <Def>
+        <main>
+            <h1>Show Page</h1>
+        </main>
+    </Def>
 }
 
 module.exports = show
