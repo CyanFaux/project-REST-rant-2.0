6d7b4251cb9c186381c46a6292b481670961dc8e
CyanFaux <santafox26@gmail.com>
2024-02-06 20:19:53 -0500

    part 9 bonus

--- controllers/places.js
+++ controllers/places.js
@@ -30,16 +30,27 @@ router.get("/:id", (req, res) => {
 });
 
 // POST
-router.post("/", (req, res) => {
-  db.Place.create(req.body)
+router.post('/', (req, res) => {
+    db.Place.create(req.body)
     .then(() => {
-      res.redirect("/places");
+        res.redirect('/places')
     })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
-});
+    .catch(err => {
+        if (err && err.name == 'ValidationError') {
+            let message = 'Validation Error: '
+            for (var field in err.errors) {
+                message += `${field} was ${err.errors[field].value}. `
+                message += `${err.errors[field].message}`
+            }
+            console.log('Validation error message', message)
+            res.render('places/new', { message })
+        }
+        else {
+            res.render('error404')
+        }
+    })
+})
+
 
 // PUT
 router.put("/:id", (req, res) => {
